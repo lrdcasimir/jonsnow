2b2318386e791301b661b68e6820fba05f6afb34
Tyler Vann-Campbell <tyler@buddy.com>
2015-05-23 20:06:21 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+var fs = require('fs');
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+fs.readFile("./gmail-app-passwd", function(e, appPasswd){
+  var transport = nodemailer.createTransport(smtpTransform({
+    service: "gmail",
+    auth: {
+      user: "tyler.v@gmail.com",
+      pass: appPasswd
+    }
+  }));
+  var knownCaptures = {};
+  var watcher = fs.watch('/tmp/motion', {persistent: true});
+  watcher.on('change', function(e, filename){
+      fs.readdir(function(e,files){
+        var newMpgFiles = files.filter(function(f){
+          return f.indexOf('mpg') > -1 && !knownCaptures[f];
+        });
+        if(newMpgFiles.length > 0){
+          newMpgFiles.forEach(function(f){
+            var mailOptions = {
+             attachments: [
+              {
+               fileName:"capture.mpg",
+               filePath:"/tmp/motion/"+f
+              }
+             ],
+             from:"tyler.v@gmail.com",
+             to:"2067472820@mms.att.net"
+            };
+            transport.sendMail(mailOptions,function(e,response){
+              if(e){
+               console.error(e);
+              } else {
+               console.log("Text message sent");
+              }
+            });
+            }
+          });
+        }
+      });
+    }
+  });
+})
+
+
